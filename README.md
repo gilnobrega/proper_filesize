9d8c2f94f348cd30379ffe63e5732da94d0fef25
Joaquim Guimaraes <jgilfnuk@gmail.com>
2021-07-26 08:10:44 +0100

    0.0.2: Updates documentation

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # proper_filesize
-a self-contained dart library that converts bytes to human readable file sizes and backwards. Also supports binary units.
+a self-contained dart library that converts bytes to human readable file sizes and backwards. 
+
+Supports binary and metric units. (KB, KiB, MB, MiB, GB, GiB, TB, TiB, PB, PiB, EB, EiB, etc.)
+
+## Usage
+Add ``proper_filesize`` as a dependency to your project's ``pubspec.yaml`` file.
+
+#### String to bytes
+
+Converts human readable string and its units to an integer number of bytes
+
+```dart
+  String metricFilesize = "1.2 EB";
+  int metricBytes = ProperFilesize.parseHumanReadableFilesize(metricFilesize).toInt();
+  print("$metricFilesize is $metricBytes bytes");
+```
+
+This will print:
+```
+1.2 EB is 1200000000000000000 bytes
+```
+
+Also works with binary units:
+
+
+```dart
+  String binaryFilesize = "1.2 EiB";
+  int binaryBytes = ProperFilesize.parseHumanReadableFilesize(binaryFilesize).toInt();
+  print("$binaryFilesize is $binaryBytes bytes");
+```
+
+This will print:
+```
+1.2 EiB is 1383505805528216320 bytes
+```
+
+#### Bytes to String
+
+Converts integer number of bytes to a human-readable filesize string
+
+```dart
+  int bytes = 1.24356e9.toInt();
+  String binaryHumanReadableFilesize = ProperFilesize.generateHumanReadableFilesize(bytes, base: Bases.Binary, decimals: 3);
+  print("$bytes bytes is $binaryHumanReadableFilesize");
+```
+
+This will print:
+```
+1243560000 bytes is 1.158 GiB
+```
--- lib/src/main.dart
+++ lib/src/main.dart
@@ -3,16 +3,16 @@ import 'dart:math' as Math;
 enum Bases { Metric, Binary }
 
 class ProperFilesize {
-  // Available units
+  /// Available units
   static const List<String> units = ['', 'K', 'M', 'G', 'T', 'P', 'E'];
 
-  // Establishes relation between type of Bases and their logarithmic value
+  /// Establishes relation between type of Bases and their logarithmic value
   static const Map<Bases, num> basesValue = {
     Bases.Metric: 1000,
     Bases.Binary: 1024
   };
 
-  // Establishes relation between type of Bases and their human readable representation
+  /// Establishes relation between type of Bases and their human readable representation
   static const Map<Bases, String> basesString = {
     Bases.Metric: 'B',
     Bases.Binary: 'iB'
@@ -21,9 +21,9 @@ class ProperFilesize {
   // converts natural logarithms to base 1000 or base 1024 logarithms
   static double _logBase(num x, num base) => Math.log(x) / Math.log(base);
 
-  // Generates a human readable string in xiB or xB from an int size in bytes
-  // Provide base (binary/metric) and decimals
-  // Defaults to binary units and 3 decimals
+  /// Generates a human readable string in xiB or xB from an int size in bytes
+  /// Provide base (binary/metric) and decimals
+  /// Defaults to binary units and 3 decimals
   static String generateHumanReadableFilesize(num size,
       {Bases base = Bases.Binary, int decimals = 3}) {
     if (size > (basesValue[base] ?? 1024)) {
@@ -37,7 +37,7 @@ class ProperFilesize {
     return "${size.toStringAsFixed(decimals)} B";
   }
 
-  // Converts a human readable filesize string (either in metric or in binary units) to an integer number of bytes
+  /// Converts a human readable filesize string (either in metric or in binary units) to an integer number of bytes
   static num parseHumanReadableFilesize(String input) {
     num size = 0;
 
