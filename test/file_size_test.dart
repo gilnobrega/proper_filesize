ec0ddc6864eb10b67e53eeb9a62277f91e0d33b9
Gil Nobrega <82336674+gilnobrega@users.noreply.github.com>
2025-01-12 21:42:16 +0000

    refactor tests

--- test/file_size_test.dart
+++ test/file_size_test.dart
@@ -2,47 +2,200 @@ import "package:proper_filesize/proper_filesize.dart";
 import "package:test/test.dart";
 
 void main() {
-  group("FileSize", () {
-    test("parse()", () {
-      expect(FileSize.parse("1 KB").size, 1000);
-      expect(FileSize.parse("1 KiB").size, 1024);
-      expect(FileSize.parse("1.2 EiB").size, 1383505805528216320);
-      expect(FileSize.parse("1.45 gibibytes").size, 1556925644.8);
+  group("FileSize.parse()", () {
+    test('''
+        SHOULD return 1000 bytes 
+        WHEN input is "1 KB"
+        ''', () {
+      // Arrange
+      const String fileSize = "1 KB";
+
+      // Act
+      final int result = FileSize.parse(fileSize).size.toInt();
+
+      // Assert
+      expect(result, 1000);
     });
 
-    test("toString()", () {
-      expect(FileSize(1000).toString(), "1000 B");
-      expect(FileSize(1024).toString(), "1 KiB");
-      expect(FileSize(1243560000).toString(), "1.158 GiB");
-      expect(
-        FileSize(1243560000).toString(
-          unit: Unit.auto(size: 1243560000, baseType: BaseType.metric),
-        ),
-        "1.244 GB",
-      );
-      expect(FileSize(1243560000).toString(decimals: 2), "1.16 GiB");
-      expect(
-        FileSize(1000).toString(
-          unit: Unit.auto(size: 1000, baseType: BaseType.metric),
-          formatType: FormatType.long,
-        ),
-        "1 kilobyte",
-      );
-      expect(
-        FileSize(1024).toString(
-          unit: Unit.auto(size: 1024, baseType: BaseType.binary),
-          formatType: FormatType.long,
-        ),
-        "1 kibibyte",
-      );
-      expect(FileSize(243560000).toString(unit: Unit.gigabyte), "0.244 GB");
+    test('''
+        SHOULD return 1024 bytes 
+        WHEN input is "1 KiB"
+        ''', () {
+      // Arrange
+      const String fileSize = "1 KiB";
+
+      // Act
+      final int result = FileSize.parse(fileSize).size.toInt();
+
+      // Assert
+      expect(result, 1024);
     });
 
-    test("toSize()", () {
-      expect(
-        FileSize(401.26, inputUnit: Unit.gigabyte).toSize(unit: Unit.mebibyte),
-        382671.3562011719,
+    test('''
+        SHOULD return 1383505805528216320 bytes 
+        WHEN input is "1.2 EiB"
+        ''', () {
+      // Arrange
+      const String fileSize = "1.2 EiB";
+
+      // Act
+      final int result = FileSize.parse(fileSize).size.toInt();
+
+      // Assert
+      expect(result, 1383505805528216320);
+    });
+
+    test('''
+        SHOULD return 1556925644 bytes 
+        WHEN input is "1.45 gibibytes"
+        ''', () {
+      // Arrange
+      const String fileSize = "1.45 gibibytes";
+
+      // Act
+      final int result = FileSize.parse(fileSize).size.toInt();
+
+      // Assert
+      expect(result, 1556925644);
+    });
+  });
+
+  group("FileSize.toString()", () {
+    test('''
+        SHOULD return "1000 B" 
+        WHEN filesize is 1000 bytes
+        ''', () {
+      // Arrange
+      const int bytes = 1000;
+
+      // Act
+      final String result = FileSize(bytes).toString();
+
+      // Assert
+      expect(result, "1000 B");
+    });
+
+    test('''
+        SHOULD return "1 KiB" 
+        WHEN filesize is 1024 bytes
+        ''', () {
+      // Arrange
+      const int bytes = 1024;
+
+      // Act
+      final String result = FileSize(bytes).toString();
+
+      // Assert
+      expect(result, "1 KiB");
+    });
+
+    test('''
+        SHOULD return "1.158 GiB" 
+        WHEN filesize is 1243560000 bytes
+        ''', () {
+      // Arrange
+      const int bytes = 1243560000;
+
+      // Act
+      final String result = FileSize(bytes).toString();
+
+      // Assert
+      expect(result, "1.158 GiB");
+    });
+
+    test('''
+        SHOULD return "1.244 GB" 
+        WHEN filesize is 1243560000 bytes and unit is metric
+        ''', () {
+      // Arrange
+      const int bytes = 1243560000;
+
+      // Act
+      final String result = FileSize(bytes).toString(
+        unit: Unit.auto(size: bytes, baseType: BaseType.metric),
       );
+
+      // Assert
+      expect(result, "1.244 GB");
+    });
+
+    test('''
+        SHOULD return "1.16 GiB" 
+        WHEN filesize is 1243560000 bytes and decimals are 2
+        ''', () {
+      // Arrange
+      const int bytes = 1243560000;
+
+      // Act
+      final String result = FileSize(bytes).toString(decimals: 2);
+
+      // Assert
+      expect(result, "1.16 GiB");
+    });
+
+    test('''
+        SHOULD return "1 kilobyte" 
+        WHEN filesize is 1000 bytes and formatType is long
+        ''', () {
+      // Arrange
+      const int bytes = 1000;
+
+      // Act
+      final String result = FileSize(bytes).toString(
+        unit: Unit.auto(size: bytes, baseType: BaseType.metric),
+        formatType: FormatType.long,
+      );
+
+      // Assert
+      expect(result, "1 kilobyte");
+    });
+
+    test('''
+        SHOULD return "1 kibibyte" 
+        WHEN filesize is 1024 bytes and formatType is long
+        ''', () {
+      // Arrange
+      const int bytes = 1024;
+
+      // Act
+      final String result = FileSize(bytes).toString(
+        unit: Unit.auto(size: 1024, baseType: BaseType.binary),
+        formatType: FormatType.long,
+      );
+
+      // Assert
+      expect(result, "1 kibibyte");
+    });
+
+    test('''
+        SHOULD return "0.244 GB" 
+        WHEN filesize is 243560000 bytes and unit is gigabyte
+        ''', () {
+      // Arrange
+      const int bytes = 243560000;
+
+      // Act
+      final String result = FileSize(bytes).toString(unit: Unit.gigabyte);
+
+      // Assert
+      expect(result, "0.244 GB");
+    });
+  });
+
+  group("FileSize.toSize()", () {
+    test("""
+        SHOULD return 382671.3562011719 
+        WHEN converting 401.26 gigabytes to mebibytes
+        """, () {
+      // Arrange
+      const num gigabytes = 401.26;
+
+      // Act
+      final num result = FileSize(gigabytes, inputUnit: Unit.gigabyte)
+          .toSize(unit: Unit.mebibyte);
+
+      // Assert
+      expect(result, 382671.3562011719);
     });
   });
 }
